470ac24a7724977d771772ec90524e9d8c32bac8
electronique <joe@joereinsel.org>
2017-07-18 15:30:53 -0400

    added p5 folder

---
+++ P5/HelloWorld!/js/sketch.js
@@ -0,0 +1,15 @@
+ function setup() {
+  createCanvas(400, 400);
+  stroke(255);
+  noFill();
+}
+
+function draw() {
+  background(0);
+  fill(255,0,0);
+  rect(100,100,200,200);
+  fill(255);
+  textAlign(CENTER);
+  textSize(30);
+  text("Hello,World",200,50);
+}
